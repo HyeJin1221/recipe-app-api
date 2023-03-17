5b6c7341ae2d8ada273f33d2a321dd12bd2b693d
HyeJin1221 <imb6457@gmail.com>
2023-03-17 19:12:22 +0900

    drf-recipe test code, user

---
+++ config/calc.py
@@ -0,0 +1,12 @@
+"""
+    
+Calculator funtions
+"""
+
+def add(x,y):
+    """Add x and y and return result."""
+    return  x+y
+
+def subtract(x,y):
+    """Subtract x from and return result."""
+    return y-x
